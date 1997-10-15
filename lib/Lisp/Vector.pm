33e133310ae14bc8da32a26f57dd80a79ab53dd4
Gisle Aas <gisle@aas.no>
1997-10-15 10:00:42 +0000

    vectorp

--- lib/Lisp/Vector.pm
+++ lib/Lisp/Vector.pm
@@ -1,5 +1,22 @@
 package Lisp::Vector;
 
+use strict;
+use vars qw(@EXPORT_OK);
+
+require Exporter;
+*import = \&Exporter::import;
+@EXPORT_OK = qw(vector vectorp);
+
+sub vector
+{
+    Lisp::Vector->new(@_);
+}
+
+sub vectorp
+{
+    UNIVERSAL::isa($_[0], "Lisp::Vector");
+}
+
 sub new
 {
     my $class = shift;
