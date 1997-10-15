6b9e4cb5bb1a8258c4e9948888175a9fcbe32bab
Gisle Aas <gisle@aas.no>
1997-10-15 10:01:15 +0000

    First revision.

---
+++ lib/Lisp/Printer.pm
@@ -0,0 +1,41 @@
+package Lisp::Printer;
+
+use Lisp::Symbol qw(symbolp);
+use Lisp::Vector qw(vectorp);
+use Lisp::Cons   qw(consp);
+
+sub dump
+{
+    require Data::Dumper;
+    Dumper($_[0]);
+}
+
+sub print
+{
+    my $obj = shift;
+    my $str = "";
+    if (ref($obj)) {
+	if (symbolp($obj)) {
+	    $str = $obj->name;
+	} elsif (vectorp($obj)) {
+	    $str = "[" . join(" ", map Lisp::Printer::print($_), @$obj) . "]";
+	} elsif (consp($obj)) {
+	    $str = "(" .join(" . ", map Lisp::Printer::print($_), @$obj). ")";
+	} else {
+	    $str = "(" . join(" ", map Lisp::Printer::print($_), @$obj) . ")";
+	}
+    } else {
+	# XXX: need real number/string type info
+	if ($obj =~ /^[+-]?\d+(?:\.\d*)?$/) {
+	    # number
+	    $str = $obj + 0;
+	} else {
+	    # string
+	    $obj =~ s/([\"\\])/\\$1/g;  # quote special chars
+	    $str = qq("$obj");
+	}
+    }
+    $str;
+}
+
+1;
