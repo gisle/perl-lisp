4217bde0d83e4390caae7a3902df21d6548749e1
Gisle Aas <gisle@aas.no>
1997-10-15 02:26:43 +0000

    symbolp

--- lib/Lisp/Symbol.pm
+++ lib/Lisp/Symbol.pm
@@ -4,7 +4,9 @@ use vars qw(@EXPORT_OK);
 
 require Exporter;
 *import = \&Exporter::import;
-@EXPORT_OK = qw(symbol);
+@EXPORT_OK = qw(symbol symbolp);
+
+#use overload '""' => \&name;
 
 my %obarray;
 
@@ -13,11 +15,21 @@ sub symbol
     Lisp::Symbol->new(@_);
 }
 
+sub symbolp
+{
+    UNIVERSAL::isa($_[0], "Lisp::Symbol");
+}
+
 sub new
 {
     my($class, $name) = @_;
     return $obarray{$name} if $obarray{$name};
     $obarray{$name} = bless \$name, $class;
+}
+
+sub name
+{
+    ${$_[0]}
 }
 
 1;
