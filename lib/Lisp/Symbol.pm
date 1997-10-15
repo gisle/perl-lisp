c02bdd8b8f40d72c4cbc108e1dc9cd7b3dced400
Gisle Aas <gisle@aas.no>
1997-10-15 14:03:45 +0000

    Implemented 'value', 'function', 'plist'.

--- lib/Lisp/Symbol.pm
+++ lib/Lisp/Symbol.pm
@@ -24,12 +24,54 @@ sub new
 {
     my($class, $name) = @_;
     return $obarray{$name} if $obarray{$name};
-    $obarray{$name} = bless \$name, $class;
+    $obarray{$name} = bless {'name' => $name}, $class;
 }
 
 sub name
 {
-    ${$_[0]}
+    $_[0]->{'name'};  # readonly
+}
+
+sub value
+{
+    my $self = shift;
+    if (defined(wantarray) || !exists $self->{'value'}) {
+	die "Symbol's value as variable is void";
+    }
+    my $old = $self->{'value'};
+    $self->{'value'} = shift if $@;
+    $old;
+}
+
+sub function
+{
+    my $self = shift;
+    if (defined(wantarray) || !exists $self->{'function'}) {
+	die "Symbol's value as function is void";
+    }
+    my $old = $self->{'function'};
+    $self->{'function'} = shift if $@;
+    $old;
+}
+
+sub plist
+{
+    my $self = shift;
+    my $old = $self->{'plist'};
+    $self->{'plist'} = shift if $@;
+    $old;
+}
+
+sub get
+{
+    my $self = shift;
+    $self->{'plist'}{$_[0]};
+}
+
+sub put
+{
+    my $self = shift;
+    $self->{'plist'}{$_[0]} = $_[1];
 }
 
 1;
