acc263449efe387f9442c6ef9f6a02b4edcc730f
Gisle Aas <gisle@aas.no>
1997-10-16 20:44:59 +0000

    First revision.

---
+++ lib/Lisp/List.pm
@@ -0,0 +1,35 @@
+package Lisp::List;
+
+use strict;
+use vars qw(@EXPORT_OK);
+
+require Exporter;
+*import = \&Exporter::import;
+@EXPORT_OK = qw(list);
+
+sub list
+{
+    Lisp::List->new(@_);
+}
+
+sub new
+{
+    my $class = shift;
+    bless [@_], $class;
+}
+
+sub print
+{
+    my $self = shift;
+    require Lisp::Printer;
+    Lisp::Printer::print($self);
+}
+
+sub eval
+{
+    my $self = shift;
+    require Lisp::Eval;
+    Lisp::Eval::eval($self);
+}
+
+1;
