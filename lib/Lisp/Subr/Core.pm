67399d710110e11809ec1cce0b3448a124489a41
Gisle Aas <gisle@aas.no>
1997-10-18 10:32:10 +0000

    Reformatting with make_special().

--- lib/Lisp/Subr/Core.pm
+++ lib/Lisp/Subr/Core.pm
@@ -4,6 +4,7 @@ package Lisp::Subr::Core;
 
 use strict;
 use Lisp::Symbol      qw(symbol);
+use Lisp::Special     qw(make_special);
 use Lisp::Reader      qw(lisp_read);
 use Lisp::Printer     qw(lisp_print);
 use Lisp::Interpreter qw(lisp_eval);
@@ -16,10 +17,12 @@ my $t      = symbol("t");
 
 sub lisp_true { defined($_[0]) && $_[0] != $nil }
 
-symbol("quote")->function(bless sub {$_[0]}, "Lisp::Special");
 symbol("list")->function(sub {[@_]});
+
+symbol("quote")->function(make_special(sub {$_[0]}));
 symbol("set")->function(sub {$_[0]->value($_[1]); $_[1]} );
-symbol("setq")->function(bless sub{my $val = lisp_eval($_[1]); $_[0]->value($val); $val}, "Lisp::Special");
+symbol("setq")->function(
+   make_special(sub{my $val = lisp_eval($_[1]); $_[0]->value($val); $val}));
 
 symbol("car")->function(sub {$_[0][0]});
 symbol("cdr")->function(
@@ -47,36 +50,35 @@ symbol("prog1")->function(sub {$_[0]});
 symbol("prog2")->function(sub {$_[1]});
 
 symbol("if")->function(
-bless
-sub {
-    my $cond = shift;
-    $cond = lisp_eval($cond);
-    if (lisp_true(lisp_eval($cond))) {
-	return lisp_eval(shift);  # then
-    }
-    shift;  # skip then-form
-    my $res;
-    for (@_) { $res = lisp_eval($_) };
-    return $res;
-
-}, "Lisp::Special");
+    make_special(
+	sub {
+	    my $cond = shift;
+	    $cond = lisp_eval($cond);
+	    if (lisp_true(lisp_eval($cond))) {
+		return lisp_eval(shift);  # then
+	    }
+	    shift;  # skip then-form
+	    my $res;
+	    for (@_) { $res = lisp_eval($_) };
+	    return $res;
+	}));
 
 symbol("cond")->function(
-bless
-sub {
-    my $res;
-    my $clause;
-    for $clause (@_) {
-	$res = lisp_eval($clause->[0]);
-	next unless lisp_true($res);
-	my $pc;
-	for ($pc = 1; $pc < @$clause; $pc++) {
-	    $res = lisp_eval($clause->[$pc]);
-	}
-	return $res;
-    }
-    undef;
-}, "Lisp::Special");
+    make_special(
+        sub {
+	    my $res;
+	    my $clause;
+	    for $clause (@_) {
+		$res = lisp_eval($clause->[0]);
+		next unless lisp_true($res);
+		my $pc;
+		for ($pc = 1; $pc < @$clause; $pc++) {
+		    $res = lisp_eval($clause->[$pc]);
+		}
+		return $res;
+	    }
+	    undef;
+	}));
 
 
 sub lisp_not { lisp_true($_[0]) ? $nil : $t }
@@ -85,37 +87,37 @@ symbol("not" )->function(\&lisp_not);
 symbol("null")->function(\&lisp_not);
 
 symbol("and")->function(
-bless
-sub {
-    my $res;
-    for (@_) {
-	$res = lisp_eval($_);
-	return $res unless lisp_true($res);
-    }
-    $res;
-}, "Lisp::Special");
+    make_special(
+        sub {
+	    my $res;
+	    for (@_) {
+		$res = lisp_eval($_);
+		return $res unless lisp_true($res);
+	    }
+	    $res;
+	}));
 
 symbol("or")->function(
-bless
-sub {
-    my $res;
-    for (@_) {
-	$res = lisp_eval($_);
-	return $res if lisp_true($res);
-    }
-    $res;
-}, "Lisp::Special");
+    make_special(
+        sub {
+	    my $res;
+	    for (@_) {
+		$res = lisp_eval($_);
+		return $res if lisp_true($res);
+	    }
+	    $res;
+	}));
 
 symbol("while")->function(
-bless
-sub {
-    my $condition = shift;
-    while (lisp_true(lisp_eval($condition))) {
-	# evaluate body
-	for (@_) { lisp_eval($_) }
-    }
-    undef;
-}, "Lisp::Special");
+    make_special(
+        sub {
+	    my $condition = shift;
+	    while (lisp_true(lisp_eval($condition))) {
+		# evaluate body
+		for (@_) { lisp_eval($_) }
+	    }
+	    undef;
+	}));
 
 # numeric functions
 symbol("floatp")->function(sub {$_[0] =~ /float/ ? $t : $nil }); #XXX
@@ -154,12 +156,12 @@ symbol("fset")->function(sub {$_[0]->function($_[1]); $_[1]});
 symbol("symbol-function")->function(sub {$_[0]->function});
 
 symbol("defun")->function(
-bless sub {
-    my $sym = shift;
-    $sym->function([$lambda, @_]);
-    $sym;
-}, "Lisp::Special"
-);
+    make_special(
+        sub {
+	    my $sym = shift;
+	    $sym->function([$lambda, @_]);
+	    $sym;
+	}));
 
 symbol("put")->function(sub{$_[0]->put($_[1] => $_[2])});
 symbol("get")->function(sub{$_[0]->get($_[1])});
@@ -167,58 +169,59 @@ symbol("get")->function(sub{$_[0]->get($_[1])});
 
 # dynamic scoping
 symbol("let")->function(
-bless
-sub {
-   my $bindings = shift;
-   my @bindings = @$bindings;  # make a copy
+    make_special(
+        sub {
+	    my $bindings = shift;
+	    my @bindings = @$bindings;  # make a copy
 
-   # First evaluate all bindings as variables
-   for my $b (@bindings) {
-       if (symbolp($b)) {
-	   $b = [$b, $nil];
-       } else {
-	   my($sym, $val) = @$b;
-	   $val = $val->value if $val && symbolp($val);
-	   $b = [$sym, $val];
-       }
-   }
+	    # First evaluate all bindings as variables
+	    for my $b (@bindings) {
+		if (symbolp($b)) {
+		    $b = [$b, $nil];
+		} else {
+		    my($sym, $val) = @$b;
+		    $val = $val->value if $val && symbolp($val);
+		    $b = [$sym, $val];
+		}
+	    }
    
-   # Then localize
-   require Lisp::Localize;
-   my $local = Lisp::Localize->new;
-   for my $b (@bindings) {
-       $local->save_and_set(@$b);
-   }
+	    # Then localize
+	    require Lisp::Localize;
+	    my $local = Lisp::Localize->new;
+	    for my $b (@bindings) {
+		$local->save_and_set(@$b);
+	    }
 
-   my $res;
-   for (@_) {
-       $res = lisp_eval($_);
-   }
-   $res;
-}, "Lisp::Special");
+	    my $res;
+	    for (@_) {
+		$res = lisp_eval($_);
+	    }
+	    $res;
+	}));
+
 
 symbol("let*")->function(
-bless
-sub {
-   my $bindings = shift;
-   require Lisp::Localize;
-   my $local = Lisp::Localize->new;
+    make_special(
+        sub {
+	    my $bindings = shift;
+	    require Lisp::Localize;
+	    my $local = Lisp::Localize->new;
 
-   # Evaluate and localize in the order given
-   for my $b (@$bindings) {
-       if (symbolp($b)) {
-	   $local->save_and_set($b, $nil);
-       } else {
-	   my($sym, $val) = @$b;
-	   $val = $val->value if $val && symbolp($val);
-	   $local->save_and_set($sym, $val);
-       }
-   }
-   my $res;
-   for (@_) {
-       $res = lisp_eval($_);
-   }
-   $res;
-}, "Lisp::Special");
+	    # Evaluate and localize in the order given
+	    for my $b (@$bindings) {
+		if (symbolp($b)) {
+		    $local->save_and_set($b, $nil);
+		} else {
+		    my($sym, $val) = @$b;
+		    $val = $val->value if $val && symbolp($val);
+		    $local->save_and_set($sym, $val);
+		}
+	    }
+	    my $res;
+	    for (@_) {
+		$res = lisp_eval($_);
+	    }
+	    $res;
+	}));
 
 1;
